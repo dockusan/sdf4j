a324d8d8e1504d62b70a53a9d5cc7effc91342ef
bancika <bancika@b77740ee-3c3f-4059-1fc8-3742f60cabb8>
2011-08-11 21:55:07 +0000

    added android test class

---  sdf4j --username bancika/sdf4j-core/test/org/slf4j/core/Test.java
+++  sdf4j --username bancika/sdf4j-core/test/org/slf4j/core/Test.java
@@ -1,8 +1,15 @@
 package org.slf4j.core;
 
 import org.sdf4j.core.Color;
+import org.sdf4j.core.Font;
 import org.sdf4j.core.ICanvas;
 
+/**
+ * Test class that draws test graphics on the provided canvas.
+ * 
+ * @see #drawSample(ICanvas)
+ * @author Branislav Stojkovic
+ */
 public class Test {
 
 	public static void drawSample(ICanvas canvas) {
@@ -14,11 +21,20 @@ public class Test {
 		canvas.drawLine(10, 10, 50, 50);
 		canvas.saveTransform();
 		canvas.rotate(Math.PI / 4, 80, 80);
-		canvas.drawRect(65, 65, 30, 30);
 		canvas.setColor(Color.yellow);
 		canvas.fillRect(65, 65, 30, 30);
+		canvas.setColor(Color.blue);
+		canvas.drawRect(65, 65, 30, 30);
 		canvas.restoreTransform();
 		canvas.setColor(Color.black);
+		canvas.setFont(new Font("Tahoma", Font.BOLD, 14));
 		canvas.drawString("test", 50, 20);
+		canvas.saveTransform();
+		canvas.scale(1.2, 0.8);
+		canvas.setColor(Color.white);
+		canvas.fillRoundRect(80, 10, 30, 30, 10, 10);
+		canvas.setColor(Color.black);
+		canvas.drawRoundRect(80, 10, 30, 30, 10, 10);
+		canvas.restoreTransform();
 	}
 }
